70387269eaaf2cd4c5eacae4d8c38278f12cc748
srossmann <sigmar.rossmann@rossmann-sotware.e>
2017-07-24 11:07:36 +0200

    Datum : 24.07.2017 Version : V5.5
    Neu : 
    1.

---
+++ JAS400Browser/src/MyRenderer.java
@@ -0,0 +1,52 @@
+
+import java.awt.Color;
+import java.awt.Component;
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableModel;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author rossmann
+ */
+//******************************************************************************
+//
+//
+//
+//******************************************************************************
+public class MyRenderer extends JLabel implements TableCellRenderer {
+
+    myTable jTable3 = null;
+    MyRenderer(myTable jTable) {
+         jTable3 = jTable;
+    }
+
+    MyRenderer(JTable jTable2) {
+        
+    }
+
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,
+            boolean hasFocus, int row, int column) {
+        //TableModel model = table.getModel();
+        setOpaque(true);
+        setBackground(row % 2 == 0 ? Color.WHITE : Color.getHSBColor(255, 255, 159));
+        setForeground(Color.BLACK);
+        setText((value != null) ? value.toString() : "");
+        
+        if (isSelected) {
+            setBorder(BorderFactory.createLineBorder(Color.BLACK));
+            //if (jTable3!=null) jTable3.fireXXX(value.toString());
+            setBackground(Color.YELLOW);
+        } else {
+            setBorder(BorderFactory.createLineBorder(table.getBackground()));
+        }
+        return this;
+    }
+}
